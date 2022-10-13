dd0258df2a2d3c0793cfa34f83857070ec338fc8
sharonsyra <sharonwaithii@gmail.com>
2022-10-13 17:04:45 +0300

    Clean up

--- kumbukumbu-api/src/main/scala/com/sharonsyra/kumbukumbu/api/KumbuKumbuService.scala
+++ kumbukumbu-api/src/main/scala/com/sharonsyra/kumbukumbu/api/KumbuKumbuService.scala
@@ -12,26 +12,26 @@ trait KumbuKumbuService extends Service {
 
   def createKumbuKumbu: ServiceCall[CreateKumbuKumbu, KumbuKumbu]
 
-//  def getKumbuKumbu(
-//    kumbuKumbuUuid: UUID
-//  ): ServiceCall[NotUsed, KumbuKumbu]
-//
-//  def searchKumbuKumbu(
-//    kumbuKumbuTitle: String
-//  ): ServiceCall[NotUsed, KumbuKumbu]
-//
-//  def updateKumbuKumbu(
-//    kumbuKumbuUuid: UUID
-//  ): ServiceCall[UpdateKumbuKumbu, KumbuKumbu]
-//
-//  def deleteKumbuKumbu(
-//    kumbuKumbuUuid: UUID
-//  ): ServiceCall[DeleteKumbuKumbu, KumbuKumbu]
-//
-//  def listKumbuKumbus: ServiceCall[NotUsed, ListKumbuKumbus]
-//
-//  // next by created at or by updated at??
-//  def nextKumbuKumbu(kumbukumbuUuid: UUID): ServiceCall[NotUsed, KumbuKumbu]
+  def getKumbuKumbu(
+    kumbuKumbuUuid: UUID
+  ): ServiceCall[NotUsed, KumbuKumbu]
+
+  def searchKumbuKumbu(
+    kumbuKumbuTitle: String
+  ): ServiceCall[NotUsed, KumbuKumbu]
+
+  def updateKumbuKumbu(
+    kumbuKumbuUuid: UUID
+  ): ServiceCall[UpdateKumbuKumbu, KumbuKumbu]
+
+  def deleteKumbuKumbu(
+    kumbuKumbuUuid: UUID
+  ): ServiceCall[DeleteKumbuKumbu, KumbuKumbu]
+
+  def listKumbuKumbus: ServiceCall[NotUsed, ListKumbuKumbus]
+
+  // next by created at or by updated at??
+  def nextKumbuKumbu(kumbukumbuUuid: UUID): ServiceCall[NotUsed, KumbuKumbu]
 
   override def descriptor: Descriptor = {
     named("kumbuKumbu")
@@ -40,37 +40,37 @@ trait KumbuKumbuService extends Service {
           method = Method.POST,
           pathPattern = "/api/kumbukumbus",
           scalaMethod = createKumbuKumbu _
+        ),
+        restCall(
+          Method.GET,
+          pathPattern = "/api/kumbukumbus/:kumbukumbuUuid",
+          scalaMethod = getKumbuKumbu _
+        ),
+        restCall(
+          Method.GET,
+          pathPattern = "/api/kumbukumbus/:kumbukumbuTitle",
+          scalaMethod = searchKumbuKumbu _
+        ),
+        restCall(
+          Method.GET,
+          pathPattern = "/api/kumbukumbus/:kumbukumbuUuid/next",
+          scalaMethod = nextKumbuKumbu _
+        ),
+        restCall(
+          Method.PATCH,
+          pathPattern = "/api/kumbukumbus/:kumbukumbuUuid",
+          scalaMethod = updateKumbuKumbu _
+        ),
+        restCall(
+          Method.DELETE,
+          pathPattern = "/api/kumbukumbus/:kumbukumbuUuid",
+          scalaMethod = deleteKumbuKumbu _
+        ),
+        restCall(
+          Method.GET,
+          pathPattern = "/api/kumbukumbus",
+          scalaMethod = listKumbuKumbus _
         )
-//        restCall(
-//          Method.GET,
-//          pathPattern = "/api/kumbukumbus/:kumbukumbuUuid",
-//          scalaMethod = getKumbuKumbu _
-//        ),
-//        restCall(
-//          Method.GET,
-//          pathPattern = "/api/kumbukumbus/:kumbukumbuTitle",
-//          scalaMethod = searchKumbuKumbu _
-//        ),
-//        restCall(
-//          Method.GET,
-//          pathPattern = "/api/kumbukumbus/:kumbukumbuUuid/next",
-//          scalaMethod = nextKumbuKumbu _
-//        ),
-//        restCall(
-//          Method.PATCH,
-//          pathPattern = "/api/kumbukumbus/:kumbukumbuUuid",
-//          scalaMethod = updateKumbuKumbu _
-//        ),
-//        restCall(
-//          Method.DELETE,
-//          pathPattern = "/api/kumbukumbus/:kumbukumbuUuid",
-//          scalaMethod = deleteKumbuKumbu _
-//        ),
-//        restCall(
-//          Method.GET,
-//          pathPattern = "/api/kumbukumbus",
-//          scalaMethod = listKumbuKumbus _
-//        )
       )
       .withAutoAcl(true)
   }
--- kumbukumbu-impl/src/main/scala/com/sharonsyra/kumbukumbu/KumbuKumbuServiceImpl.scala
+++ kumbukumbu-impl/src/main/scala/com/sharonsyra/kumbukumbu/KumbuKumbuServiceImpl.scala
@@ -2,23 +2,18 @@ package com.sharonsyra.kumbukumbu
 
 import akka.NotUsed
 import com.lightbend.lagom.scaladsl.api.ServiceCall
-import com.sharonsyra.common.{
-  CreateKumbuKumbu,
-  DeleteKumbuKumbu,
-  KumbuKumbu,
-  ListKumbuKumbus,
-  TimeStamp,
-  UpdateKumbuKumbu
-}
+import com.sharonsyra.common._
 import com.sharonsyra.kumbukumbu.api.KumbuKumbuService
 
 import java.time.Instant
 import java.util.UUID
-import scala.concurrent.{ExecutionContext, Future}
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.Future
 
-class KumbuKumbuServiceImpl
-//(implicit ec: ExecutionContext)
-    extends KumbuKumbuService {
+class KumbuKumbuServiceImpl extends KumbuKumbuService {
+
+  val kumbuKumbus: ArrayBuffer[KumbuKumbu] = ArrayBuffer[KumbuKumbu]()
+
   override def createKumbuKumbu: ServiceCall[CreateKumbuKumbu, KumbuKumbu] =
     ServiceCall { request =>
       val kumbuKumbuUuid = UUID.randomUUID()
@@ -35,27 +30,29 @@ class KumbuKumbuServiceImpl
           updatedAt = None
         )
 
-      println(s"New KumbuKumbu Created: Details -> $newKumbuKumbu")
+      kumbuKumbus.+=(newKumbuKumbu)
+
+      println(s"Kumbukumbus are -> $kumbuKumbus")
 
       Future.successful(
         newKumbuKumbu
       )
     }
 
-//  override def getKumbuKumbu(
-//    kumbuKumbuUuid: UUID): ServiceCall[NotUsed, KumbuKumbu] = ???
-//
-//  override def searchKumbuKumbu(
-//    kumbuKumbuTitle: String): ServiceCall[NotUsed, KumbuKumbu] = ???
-//
-//  override def updateKumbuKumbu(
-//    kumbuKumbuUuid: UUID): ServiceCall[UpdateKumbuKumbu, KumbuKumbu] = ???
-//
-//  override def deleteKumbuKumbu(
-//    kumbuKumbuUuid: UUID): ServiceCall[DeleteKumbuKumbu, KumbuKumbu] = ???
-//
-//  override def listKumbuKumbus: ServiceCall[NotUsed, ListKumbuKumbus] = ???
-//
-//  override def nextKumbuKumbu(
-//    kumbukumbuUuid: UUID): ServiceCall[NotUsed, KumbuKumbu] = ???
+  override def getKumbuKumbu(
+    kumbuKumbuUuid: UUID): ServiceCall[NotUsed, KumbuKumbu] = ???
+
+  override def searchKumbuKumbu(
+    kumbuKumbuTitle: String): ServiceCall[NotUsed, KumbuKumbu] = ???
+
+  override def updateKumbuKumbu(
+    kumbuKumbuUuid: UUID): ServiceCall[UpdateKumbuKumbu, KumbuKumbu] = ???
+
+  override def deleteKumbuKumbu(
+    kumbuKumbuUuid: UUID): ServiceCall[DeleteKumbuKumbu, KumbuKumbu] = ???
+
+  override def listKumbuKumbus: ServiceCall[NotUsed, ListKumbuKumbus] = ???
+
+  override def nextKumbuKumbu(
+    kumbukumbuUuid: UUID): ServiceCall[NotUsed, KumbuKumbu] = ???
 }
